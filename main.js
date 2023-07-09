d7788e3213b261f7f8e8cc35714852011ede1872
Wilhelm <47674972+elmWilh@users.noreply.github.com>
2023-07-09 23:05:32 +0200

    fix bugs and new skin

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require('electron');
 const { Client, Authenticator } = require('minecraft-launcher-core');
+const os = require('os');
 const path = require('path');
-const fs = require('fs');
 
 let launcher;
 let win;
+let consoleWindow;
 
 function createWindow() {
   win = new BrowserWindow({
@@ -18,12 +19,16 @@ function createWindow() {
       nodeIntegration: true,
       contextIsolation: false,
       enableRemoteModule: true,
+      devTools: false,
     },
   });
 
   win.loadFile('index.html');
   win.on('closed', () => {
     win = null;
+    if (consoleWindow) {
+      consoleWindow.close();
+    }
   });
 }
 
@@ -31,7 +36,7 @@ function createLauncher() {
   let launcherOptions = {
     clientPackage: null,
     authorization: Authenticator.getAuth('player'),
-    root: path.join('C:', 'Users', 'atiki', 'AppData', 'Roaming', '.minecraft'),
+    root: path.join(os.homedir(), 'AppData', 'Roaming', '.minecraft'),
     version: {
       number: '1.20',
       type: 'release',
@@ -44,22 +49,22 @@ function createLauncher() {
 
   launcher = new Client();
 
-  const logPath = path.join(__dirname, 'logs', 'latest.log');
-  fs.mkdirSync(path.dirname(logPath), { recursive: true });
-
   launcher.on('debug', (message) => {
-    fs.appendFileSync(logPath, `[DEBUG] ${message}\n`);
     console.log(message);
+    consoleWindow.webContents.send('consoleLog', `[DEBUG] ${message}`);
   });
 
   launcher.on('data', (message) => {
-    fs.appendFileSync(logPath, `[DATA] ${message}\n`);
     console.log(message);
-
-    const match = message.match(/(?<=\[[\d:]+\] \[Client thread\/INFO]: \[minecraft\/Bootstrap\]).*?(\d+)%/);
-    if (match) {
-      const progress = parseInt(match[1]);
-      win.webContents.send('updateGameLaunchProgress', progress);
+    consoleWindow.webContents.send('consoleLog', `[DATA] ${message}`);
+  
+    // Identify and update game launch progress based on the console logs
+    if (message.includes('MCLC version 3.17.1')) {
+      win.webContents.send('updateGameLaunchProgress', 10);
+    } else if (message.includes('Downloaded assets')) {
+      win.webContents.send('updateGameLaunchProgress', 50);
+    } else if (message.includes('Launching with arguments')) {
+      win.webContents.send('updateGameLaunchProgress', 100);
     }
   });
 
@@ -68,26 +73,40 @@ function createLauncher() {
     launcherOptions.memory.max = `${ramAllocation}G`;
     console.log(`Launching Minecraft as ${username} with ${ramAllocation}GB of RAM`);
 
-    win.webContents.send('updateGameLaunchProgress', 0);
-
-    launcher.launch(launcherOptions, (err, child) => {
-      if (err) {
-        console.error('Failed to launch Minecraft', err);
-        return;
-      }
-
-      child.on('close', (code) => {
-        const success = code === 0;
-        win.webContents.send('gameLaunchFinished', success);
-
-        // Fill up the progress bar to 100% once the game is launched
-        win.webContents.send('updateGameLaunchProgress', 100);
-      });
-    });
+    launcher.launch(launcherOptions);
   });
 }
 
-app.whenReady().then(createWindow).then(createLauncher);
+function createConsoleWindow() {
+  consoleWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    resizable: false,
+    frame: false,
+    show: false,
+    icon: path.join(__dirname, 'icons/dev.png'),
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+      devTools: false,
+    },
+  });
+
+  consoleWindow.loadFile('console.html');
+  consoleWindow.once('ready-to-show', () => {
+    consoleWindow.show();
+  });
+
+  consoleWindow.on('closed', () => {
+    consoleWindow = null;
+  });
+}
+
+app.whenReady()
+  .then(createWindow)
+  .then(createLauncher)
+  .then(createConsoleWindow);
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
--- renderer.js
+++ renderer.js
@@ -6,33 +6,44 @@ const ramAmount = document.getElementById('ramAmount');
 const launchButton = document.getElementById('launchMinecraftButton');
 const gameLoadProgressBar = document.getElementById('gameLoadProgress');
 const gameLoadProgressValue = document.getElementById('gameLoadProgressValue');
+const openConsoleButton = document.getElementById('openConsoleButton');
 
 ramSlider.addEventListener('input', function () {
-    ramAmount.textContent = `${ramSlider.value} GB`;
+  ramAmount.textContent = `${ramSlider.value} GB`;
 });
 
 launchButton.addEventListener('click', function () {
-    let username = usernameInput.value;
-    let ramAllocation = ramSlider.value;
-
-    if (username.trim() === '') {
-        alert('Нужно ввести ник в строку с ником.');
-        return;
-    }
-
-    ipcRenderer.send('launchMinecraft', username, ramAllocation);
+  let username = usernameInput.value;
+  let ramAllocation = ramSlider.value;
+  ipcRenderer.send('launchMinecraft', username, ramAllocation);
 });
 
 ipcRenderer.on('gameLaunchFinished', () => {
-    gameLoadProgressBar.style.width = "100%";
-    gameLoadProgressValue.textContent = "100%";
+  gameLoadProgressBar.style.width = "100%";
+  gameLoadProgressValue.textContent = "100%";
+});
+
+ipcRenderer.on('updateGameLaunchProgress', (event, progress) => {
+  gameLoadProgressBar.style.width = `${progress}%`;
+  gameLoadProgressValue.textContent = `${progress}%`;
+});
+
+openConsoleButton.addEventListener('click', () => {
+  ipcRenderer.send('openConsole');
+});
+
+ipcRenderer.on('consoleLog', (event, log) => {
+  const consoleOutput = document.getElementById('consoleOutput');
+  const logLine = document.createElement('div');
+  logLine.textContent = log;
+  consoleOutput.appendChild(logLine);
 });
 
 document.getElementById('changeUsername').addEventListener('click', () => {
-    const username = usernameInput.value;
-    ipcRenderer.send('changeUsername', username);
+  const username = usernameInput.value;
+  ipcRenderer.send('changeUsername', username);
 });
 
 document.getElementById('installVersion').addEventListener('click', () => {
-    ipcRenderer.send('installVersion');
+  ipcRenderer.send('installVersion');
 });
